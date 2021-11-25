1808abc4d221dd647ce340694eac3e0d2b4fcb78
b3nnnn <35284611+b3nnnn@users.noreply.github.com>
2021-11-25 14:46:29 +1100

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,10 @@ def apply_header(response):
 @app.route('/<path:u_path>')
 def catch_all(u_path):
     app.logger.critical(request.headers)
+    app.logger.critical(request.data)
+    app.logger.critical(request.args)
+    app.logger.critical(request.form)
+    app.logger.critical(request.endpoint)
+    app.logger.critical(request.method)
+    app.logger.critical(request.remote_addr)
     return "Yeah Nah"
